e0ac48801fc8d57b87589121444e940da36256e1
marcomammi02 <marcomammi2002@gmail.com>
2023-01-23 23:45:34 +0100

    Makeing routes

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,12 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', 'AdminPagesController@dasboard')->name('admin.dashboard'); // Homepage dell'admin (Dashboard)
+
+Route::get('/comics', 'ComicController@index')->name('comics.index');
+Route::post('/comics', 'ComicController@store')->name('comics.store');
+Route::get('/comics/create', 'ComicController@create')->name('comics.create');
+Route::get('/comics/{comic}', 'ComicController@show')->name('comics.show');
+Route::put('/comics/{comic}', 'ComicController@update')->name('comics.update');
+Route::get('/comics/{comic}/edit', 'ComicController@edit')->name('comics.edit');
+Route::delete('/comics/{comic}', 'ComicController@destroy')->name('comics.destroy');
